00624b6cd4573ce07e28ba05f33634a7490b37e9
EvanCastner <ekc30@uakron.edu>
2025-12-16 18:42:47 +0000

    Add dummy data into memory.cpp

--- src/monitor/memory.cpp
+++ src/monitor/memory.cpp
@@ -1,5 +1,18 @@
 #include "memory.hpp"
+#include <string>
 
 namespace monitor {
-	float get_memory_usage() { return 0.0f; }
+
+	static float t = 0.0f;
+
+	void update_memory(MemoryData& mem) {
+		// Dummy RAM data
+		mem.totalMB = 16000.0f;
+
+		// Dummy usage oscillation
+		mem.usagePercent = 50.0f + 30.0f * std::sin(t);
+		t += 0.03f;
+
+		mem.usedMB = mem.totalMB * (mem.usagePercent / 100.0f);
+	}
 }
--- src/monitor/memory.hpp
+++ src/monitor/memory.hpp
@@ -1,4 +1,5 @@
 #pragma once 
+#include <vector>
 
 namespace moniter {
 	struct MemoryData {
