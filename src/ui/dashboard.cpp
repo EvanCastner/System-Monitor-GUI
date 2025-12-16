2a3507990b3e088e2229465fed9ff8c104ec5965
Evan Castner <evancastner@Mac.lan>
2025-12-15 19:38:10 -0500

    Add dummy data for cpu usage and history

--- src/ui/dashboard.cpp
+++ src/ui/dashboard.cpp
@@ -1,10 +1,44 @@
 #include "dashboard.hpp"
+
 #include "imgui.h"
+#include "implot.h"
+
+#include <vector>
+#include <cmath>
+
 
 namespace ui {
 	void render_dashboard() {
-		ImGui::Begin("dashboard");
-		ImGui::Text("Dashboard placeholder");
+		static float t = 0.0f;
+		static std::vector<float> cpu_history;
+		static const int MAX_SAMPLES = 200;
+
+		// Generate dummy CPU usage
+		flaot cpu_usage = 50.0f + 40.0f * std::sin(t);
+		t = 0.05f;
+
+		cpu_history.push_back(cpu_usage);
+		if (cpu_history.size() > MAX_SAMPLES) {
+			cpu_history.erase(cpu_history.begin());
+		}
+
+		ImGui::Begin("System Moniter Dashboard");
+
+		ImGui::Text("CPU Usage Dummy Data");
+		ImGui::Seperator();
+
+		if (ImPlot::BeginPlot("CPU Usage %", ImVec2(-1, 200))) {
+			ImPlot::SetupAxes(nullptr, "Usage %", ImPlotAxisFlags_NoTickLabels, ImPlotAxisFlags_AutoFit);
+			ImPlot::SetupAxisLimits(ImAxis_Y1, 0, 100. ImGuiCond_Always);
+
+			ImPlot::PlotLine("CPU %", cpu_history.data(), cpu_history.size());
+
+			ImPlot::EndPlot();
+		}
+
+		ImGui::Text("Memory Usage: TODO");
+		ImGui::Text("Network Usage: TODO");
+
 		ImGui::End();
 	}
 }
