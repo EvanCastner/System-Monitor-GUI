ac49f69a58c366e82ab4f632a6bb53e63e580901
Evan Castner <evancastner@Mac.lan>
2026-01-05 16:10:07 -0500

    Delete build from github page

--- build/CMakeFiles/SystemMonitor.dir/src/monitor/network.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/SystemMonitor.dir/src/monitor/network.cpp.o: \
-  /Users/evancastner/Personal/System-Monitor-GUI/src/monitor/network.cpp \
-  /Users/evancastner/Personal/System-Monitor-GUI/src/monitor/network.hpp
