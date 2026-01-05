0c4a2ecb1f32a405d6ab43a5aeb37127d4bbc873
Evan Castner <evancastner@Mac.lan>
2026-01-05 16:39:35 -0500

    Fix spelling mistakes

--- src/monitor/network.hpp
+++ src/monitor/network.hpp
@@ -1,11 +1,16 @@
 #pragma once 
 
+#include <vector>
+
 namespace monitor {
 	struct NetworkData {
-		float downloaidnKBps;
+		float downloadKBps;
 		float uploadKBps;
 		float totalDownloadMB;
 		float totalUploadMB;
+
+		std::vector<float> downloadHistory;
+		std::vector<float> uploadHistory;
 	};
 
 	void update_network(NetworkData& net);
