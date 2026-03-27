145e8480562da1aec99a88560d113f38dc5d27f3
Evan Castner <evancastner@Mac.lan>
2026-03-27 10:08:55 -0400

    Add comments and dummy data for windows detected systems

--- src/monitor/network.cpp
+++ src/monitor/network.cpp
@@ -22,30 +22,20 @@
 
 namespace monitor {
 
-	static float t =0.0f;
+	// Maximum number of historical network samples that will be retained
+	static const int MAX_SAMPLE = 120;
 
+	/** 
+	 * Updates network usage statistics for the given NetworkData object
+	 * Platform-specific implementation using native APIs
+	 * @param net Reference to NetworkData structure to update
+	 */
 	void update_network(NetworkData& net) {
-		// Simulated speeds (KB/s)
-		net.downloadKBps = 500.0f + 400.0f * std::sin(t);
-		net.uploadKBps = 120.0f + 100.0f * std::cos(t * 0.08f);
 
-		// Prevent negative numbers
-		if (net.downloadKBps < 0.0f) net.downloadKBps = 0.0f;
-		if (net.uploadKBps < 0.0f) net.uploadKBps = 0.0f;
+		#if defined(_WIN32)
+			// Dummy data for windows
+			net.downloadKBps = 0.0f;
+			net.uploadKbps   = 0.0f;
 
-		// Simulated totals
-		net.totalDownloadMB += net.downloadKBps / 1024.0f / 60.0f;
-		net.totalUploadMB += net.uploadKBps / 1024.0f / 60.0f;
-
-		// Show history buffs
-		net.downloadHistory.push_back(net.downloadKBps);
-		net.uploadHistory.push_back(net.uploadKBps);
-
-		if (net.downloadHistory.size() > 120) {
-			net.downloadHistory.erase(net.downloadHistory.begin());
-			net.uploadHistory.erase(net.uploadHistory.begin());
-		}
-
-		t += 0.05;
 	}
 }
